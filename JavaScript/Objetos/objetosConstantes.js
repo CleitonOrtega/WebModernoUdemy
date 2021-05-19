6c47b208f79ac158a8b83a45c89e19b95c607fc3
C. Ortega <c.ortega200935@gmail.com>
2021-05-19 19:11:43 -0300

    Aulas sobre OO, Revisão Objetos, Estrategias de criação de Objetos e Objetos constantes

---
+++ JavaScript/Objetos/objetosConstantes.js
@@ -0,0 +1,23 @@
+/*
+ *Aula sobre objetos constantes 
+ */
+//pessoa -> 123 -> {...}
+const pessoa = {nome: 'João'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+//pessoa -> 456 -> {...}
+//pessoa = {nome: 'Ana'}
+
+//Congela o objeto, fica sem conseguir mexer nele
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({nome: 'João'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
---
+++ JavaScript/Objetos/oo.js
@@ -0,0 +1,27 @@
+/*
+Aula sobre introdução a OO
+Aula com códigos não executaveis apenas teoria! 
+*/
+
+//procedural
+processamento(valor1,valor2,valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() //foco passou a ser objetos
+
+/*
+Principios importantes:
+1. Abstração
+2. Encapsulamento
+3. Herança (prototype) 
+4. Polimorfismo
+*/
